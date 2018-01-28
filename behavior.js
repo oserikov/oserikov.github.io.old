28b77d772ce6f5a4f57d1503adca7514316a6103
oserikov <olegserikov@mail.ru>
2018-01-28 17:05:58 +0300

    contents fix

--- behavior.js
+++ behavior.js
@@ -54,15 +54,34 @@ function prettyResizeBottomMarginOfContentContainer() {
     var tmp = document.getElementById('content_container');
 
     var lastHeight = divs[divs.length - 1].getBoundingClientRect().bottom - divs[divs.length - 1].getBoundingClientRect().top;
-    tmp.style.marginBottom = ($(window).height() - lastHeight - parseFloat(getComputedStyle(tmp).fontSize)).toString() + "px";
+    tmp.style.marginBottom = ($(window).height() - lastHeight).toString() + "px";
     console.log("ENDED AAA");
 
+}
+
+function checkOverflow() {
+    var element = document.getElementById("nav_side_bar");
+
+    if (element.scrollHeight > element.clientHeight){
+        document.getElementById("nav_side_bar_menu").style.display = 'none';
+    }
+    else {
+        if (document.getElementById("nav_side_bar_menu").style.display === 'none'){
+            var p = $('#nav_side_bar_menu').clone().css('display', 'none');
+            $('body').append(p);
+            if ($('#nav_side_bar_contacts').height() + $('#nav_side_bar_content').height() + p.height() < $(window).height()){
+                document.getElementById("nav_side_bar_menu").style.display = 'block';
+            }
+            p.remove();
+        }
+    }
 }
 
 
 $(window).scroll(function () {
     console.log("Window.Scroll() STARTED");
     prettySideNavBarHandling();
+    checkOverflow();
     console.log("Window.Scroll() FINISHED");
 });
 
@@ -76,13 +95,16 @@ window.onload = function () {
 
 window.onresize = function () {
     prettyResizeBottomMarginOfContentContainer();
-}
+    checkOverflow();
+    GetContainerSize();
+};
 
 $(window).on('hashchange', function () {
     console.log("Window.OnHashChange() STARTED");
     if (document.readyState === "complete") {
         prettySideNavBarHandling();
     }
+    checkOverflow();
     console.log("Window.OnHashChange() FINISHED");
 });
 
